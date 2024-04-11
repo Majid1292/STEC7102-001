a4fcf1ab824d147c91ed59446386a6feb31c5dd4
Majid <49441325+Majid1292@users.noreply.github.com>
2024-04-11 19:50:41 -0400

    Manipulate DOM

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 "use strict";
-let nodes = document.querySelectorAll("section>ul>li");
-console.log(nodes);
-for (let item of nodes) {
-    console.log(item.textContent);
+let node = document.querySelector("section>h1");
+node.className = "brown";
+node.classList.add("box")
+console.log(node.classList);
+node.classList.remove("box")
 
-}
+node.textContent= "Web Development Class";
+
+let h2 = document.createElement("h3");
+let content = document.createTextNode("Course STEC 7102-001");
+
+h2.appendChild(content);
+let node_section = document.querySelector("section.content");
+console.log(node_section);
+node_section.appendChild(h2);
+
+
+
 
