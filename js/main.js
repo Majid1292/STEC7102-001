37fed39ad598518c83a5c0adc6e0cea4e4871b95
Majid <49441325+Majid1292@users.noreply.github.com>
2024-04-15 18:14:18 -0400

    Add EventListener

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 "use strict";
-let node = document.querySelector("section>h1");
-node.className = "brown";
-node.classList.add("box")
-console.log(node.classList);
-node.classList.remove("box")
 
-node.textContent= "Web Development Class";
-
-let h2 = document.createElement("h3");
-let content = document.createTextNode("Course STEC 7102-001");
-
-h2.appendChild(content);
-let node_section = document.querySelector("section.content");
-console.log(node_section);
-node_section.appendChild(h2);
+const button = document.querySelector('button');
+button.addEventListener('click',showMore);
 
 
+function showMore() {
+    let p = document.createElement("p");
+    let content = document.createTextNode("This is more information about U of Windsor");
+    p.appendChild(content);
 
-
+    let article = document.querySelector("article");
+    article.appendChild(p);
+    button.removeEventListener('click',showMore);
+}
