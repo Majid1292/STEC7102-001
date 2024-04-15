4ea284fd9af7e5d30dd4914794c608d27e928612
Majid <49441325+Majid1292@users.noreply.github.com>
2024-04-15 19:01:52 -0400

    Mouse&keyboard events

--- js/main.js
+++ js/main.js
@@ -13,3 +13,36 @@ function showMore() {
     article.appendChild(p);
     button.removeEventListener('click',showMore);
 }
+
+window.addEventListener('keydown', function(event) {
+    if (event.key=='b') {
+        this.document.body.style.background = "skyblue";
+    }
+})
+window.addEventListener('keyup', event => {
+    if (event.key=='b') {
+        this.document.body.style.background = "";
+    }
+})
+
+let bar = document.querySelector("footer>div");
+let lastX;
+bar.addEventListener("mousedown", event => {
+    if (event.button == 0) {
+        lastX = event.clientX;
+        window.addEventListener("mousemove", moved);
+        event.preventDefault;
+    } 
+})
+
+function moved(event) {
+    if (event.button==0) {
+        window.removeEventListener("mousemove",moved);
+    } else {
+        let distance = event.clientX - lastX;
+        let newWidth = bar.offsetWidth + distance;
+        bar.style.width = newWidth + "px";
+        lastX = event.clientX;
+    }
+
+}
